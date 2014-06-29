f1717809ace543cd5cc670769dad931696a9f1b8
Clément Hémidy <clement@hemidy.fr>
2014-06-29 20:09:21 +0200

    change config for production

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'worldcup',
 			'username'  => 'worldcup',
-			'password'  => 'fcote.loc',
+			'password'  => 'fcotechemidy8nk',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
